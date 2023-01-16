118d38b0503b640fb4684e802dab56b47044221f
Nagham Abdelrahman <naghamabdelrahman62@gmail.com>
2023-01-16 09:23:14 -1200

    added shared preference

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:todo/providers/settings_provider.dart';
 import 'package:todo/ui/edit_task_screen.dart';
 import 'package:todo/ui/home/home_screen.dart';
@@ -16,9 +17,12 @@ void main() async {
 }
 
 class MyApp extends StatelessWidget {
+  late SettingsProvider settingsProvider;
+
   @override
   Widget build(BuildContext context) {
-    var settingsProvider = Provider.of<SettingsProvider>(context);
+    settingsProvider = Provider.of<SettingsProvider>(context);
+    initSharedPref();
     return MaterialApp(
       theme: MyTheme.lightTheme,
       darkTheme: MyTheme.darkTheme,
@@ -38,5 +42,17 @@ class MyApp extends StatelessWidget {
       initialRoute: HomeScreen.routeName,
       debugShowCheckedModeBanner: false,
     );
+  }
+
+  initSharedPref() async {
+    final prefs = await SharedPreferences.getInstance();
+    String lang = prefs.getString('language') ?? 'en';
+    settingsProvider.changeLanguage(lang);
+    String? theme = prefs.getString('theme');
+    if (theme == 'light') {
+      settingsProvider.changeTheme(ThemeMode.light);
+    } else if (theme == 'dark') {
+      settingsProvider.changeTheme(ThemeMode.dark);
+    }
   }
 }
--- lib/providers/settings_provider.dart
+++ lib/providers/settings_provider.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class SettingsProvider extends ChangeNotifier {
   ThemeMode currentTheme = ThemeMode.light;
   String currentLanguage = 'en';
 
-  void changeTheme(ThemeMode newMode) {
+  void changeTheme(ThemeMode newMode) async {
     currentTheme = newMode;
+    final prefs = await SharedPreferences.getInstance();
+    prefs.setString(
+        'theme', currentTheme == ThemeMode.light ? 'light' : 'dark');
     notifyListeners();
   }
 
@@ -13,8 +17,10 @@ class SettingsProvider extends ChangeNotifier {
     return currentTheme == ThemeMode.light;
   }
 
-  void changeLanguage(String newLanguage) {
+  void changeLanguage(String newLanguage) async {
     currentLanguage = newLanguage;
+    final prefs = await SharedPreferences.getInstance();
+    prefs.setString('language', currentLanguage);
     notifyListeners();
   }
 }
